61c580d7edce031fd1c11358bd2138588923fdef
Jakub Strawa <j.strawa@stud.elka.pw.edu.pl>
2019-06-05 22:01:18 +0200

    Todo deleted

--- main.cpp
+++ main.cpp
@@ -1,47 +1,6 @@
 
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// In order to load the resources like cute_image.png, you have to set up
-// your target scheme:
-//
-// - Select "Edit Scheme…" in the "Product" menu;
-// - Check the box "use custom working directory";
-// - Fill the text field with the folder path containing your resources;
-//        (e.g. your project folder)
-// - Click OK.
-//
-// TODO:
-//     + change textures - different cherry and snake, maybe background --- red is for candy
-//     + make snake head texture, and snake body rotate
-//     + make corners on snake body
-//     ? can go through walls - should stay
-//     + can be stuck in wall - seems it is drawing one row less
-//     + can change direction by 180
-//     + add score counter
-//     + add possibilities to lose - eating itself, ? hitting wall
-//     + add quiting by ESC
-//     + check spawning cherry where snake is
-//     + add music
-//     + set game icon
-//     + remake main to have bool isRunning
-//     + one class Game with all objects and functions
-//     + add winning possibility
-//  Add project objectives:
-//     + Funkcje wirtualne - klasa obiekt dla płytek z funk draw
-//     + Dziedziczenie - klasa główna obiekt, dziedziczące- tło, snake, candy
-//     + Wyjątki - może przechodzenie przez ściany, albo candy w snake'u, throw klase specjalną, może być pusta
-//     + Testy jednostkowe - gettery, settery, przeliczanie pozycji
-//
-// BUGS/GLITCHES:
-//     + candy can spawn in snake
-//     + candy won't spawn at all (with small maps)
-//     + snake can move inside itself
-
-
+// Snake game
+// CC Jakub Strawa 2019
 
 
 //  DIRECTIONS:
