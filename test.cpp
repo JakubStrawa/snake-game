82fcefe93a9cefe877fa791e308781ab3f4b20f0
Jakub Strawa <j.strawa@stud.elka.pw.edu.pl>
2019-06-05 18:39:15 +0200

    Unit test wip

---
+++ test.cpp
@@ -0,0 +1,11 @@
+//
+//  test.cpp
+//  Snake
+//
+//  Created by Jakub Strawa on 05/06/2019.
+//  Copyright © 2019 Jakub Strawa. All rights reserved.
+//
+//  Unit Tests
+
+#include <stdio.h>
+#include "catch.hpp"
